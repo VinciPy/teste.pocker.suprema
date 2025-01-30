593df96c12003dbd88fad67b282b1362434cab67
vinci <viniciusantana323@gmail.com>
2025-01-30 15:24:22 -0300

    test: use case poker table

--- src/test/java/org/acme/PokerTableTest.java
+++ src/test/java/org/acme/PokerTableTest.java
@@ -2,16 +2,34 @@ package org.acme;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.suprema.application.usecases.CreatePokerTable.CreatePokerTableInteractor;
 import org.suprema.domain.entities.Player;
 import org.suprema.domain.entities.PokerTable;
+import org.suprema.infra.gateways.PokerTableRepositoryGateway;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
 @QuarkusTest
 public class PokerTableTest {
+    @Mock
+    private PokerTableRepositoryGateway pokerTableRepositoryGateway;
+
+    @InjectMocks
+    private CreatePokerTableInteractor createPokerTableInteractor;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
     @DisplayName("should be a instance of poker table")
     public void testPokerTableEntity() {
@@ -135,5 +153,47 @@ public class PokerTableTest {
         pokerTable.addPlayer(player3);
         pokerTable.addPlayer(player4);
         Assertions.assertTrue(pokerTable.isValidTable());
+    }
+
+    @Test
+    @DisplayName("Should be test a use case of create poker table")
+    public void testUseCaseCreatePokerTable() {
+        PokerTable pokerTableMock = new PokerTable(
+                "pk_mock",
+                new ArrayList<>()
+        );
+        Mockito.when(pokerTableRepositoryGateway.createPokerTable(Mockito.any(PokerTable.class)))
+                .thenReturn(pokerTableMock);
+        PokerTable result = this.createPokerTableInteractor.createPokerTable(pokerTableMock);
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(result.getId(), pokerTableMock.getId());
+    }
+
+
+    @Test
+    @DisplayName("Should be test a use case of add players different")
+    public void testUseCaseAddPlayerDifferentsPokerTable() {
+        PokerTable pokerTableMock = new PokerTable(
+                "pk_mock",
+                new ArrayList<>()
+        );
+        Player playerMock1 = new Player(
+                "player_mock1",
+                "111.111.111-23",
+                "11 11111-1111",
+                "12312"
+        );
+        Player playerMock2 = new Player(
+                "player_mock1",
+                "111.111.111-23",
+                "11 11111-1111",
+                "12312"
+        );
+        Mockito.when(pokerTableRepositoryGateway.createPokerTable(Mockito.any(PokerTable.class)))
+                .thenReturn(pokerTableMock);
+        PokerTable result = this.createPokerTableInteractor.createPokerTable(pokerTableMock);
+        pokerTableMock.addPlayer(playerMock1);
+        pokerTableMock.addPlayer(playerMock2);
+        Assertions.assertEquals(result.getId(), pokerTableMock.getId());
     }
 }
